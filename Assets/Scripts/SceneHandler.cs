cacaebaff3bb4743a27067b1e6033f2b7da7e934
Nathan G <nathan.gooneratne@gmail.com>
2022-10-01 19:00:41 -0400

    WIP UI + Scene Transitions

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public static void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index, LoadSceneMode.Single);
+    }
+
+    public void StartGame()
+    {
+        StartCoroutine(WaitForAnim());
+    }
+
+    IEnumerator WaitForAnim()
+    {
+        yield return new WaitForSeconds(1.2f);
+        
+        LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quitted");
+        Application.Quit();
+    }
+}
